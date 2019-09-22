0eb1fe3b6f59599dd7137e8ac89d88dcb4d49e85
liwenxian <642091886@qq.com>
2019-09-22 20:09:50 +0800

    123

---
+++ helloProject/HelloScala.scala
@@ -0,0 +1,8 @@
+package com.itmumu
+
+object HelloScala {
+  def main(args: Array[String]): Unit = {
+    println("helloworld")
+  }
+
+}
---
+++ helloProject/MethodDemo1.scala
@@ -0,0 +1,14 @@
+package com.itmumu
+
+object MethodDemo1 {
+  def main(args: Array[String]): Unit = {
+
+  }
+  def method1(num: Int): Unit = {
+    println(num)
+  }
+  def getsum(x:Int,y:Int):Int={
+    val result=x+y
+    result
+  }
+}
